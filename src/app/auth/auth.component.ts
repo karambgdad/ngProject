48ebf3eb5c4bae6e6c681d302eec39546847f0c2
karam <karambdad@gmil.com>
2023-06-16 15:28:02 +0200

    auth-301

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
 import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+
+
+//now we need forwarding the user to a different route once the user is logged in. Now since we always want to redirect the user once we authenticate it, we of course can do this in two different places. We can either do it in the service and handle authentication or we do it in the component here, in the subscribe in the success case only Of course.
 
 @Component({
   selector: "app-auth",
@@ -14,7 +18,8 @@ export class AuthComponent implements OnInit {
 
   authForm: FormGroup;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private router: Router) {}
 
   ngOnInit() {
     this.authForm = new FormGroup({
@@ -40,40 +45,21 @@ export class AuthComponent implements OnInit {
     let authObs : Observable<AuthResponseData>
 
     this.isLoading = true;
-    //right before the request get sent, we change isLoading to true, because now we are sending a request so we are currenly loading, and i set this back to false as soon as i am done.
     if (this.isLoginMode) {
          
        authObs=  this.authService.login(email,password)
     } else {
 
        authObs= this.authService.signUp(email, password)
-
-      //////////////very IMPORTANT///////////////////////////////////////////
-      //this.authService.signUp(email, password).subscribe({// we subscribe to the success result of the http post observalbe with next: , and we subscribe to the error result of the http post observable with error:
-      //  next : (data) => {
-      //   console.log(data);
-      //    this.isLoading = false// we are not loading any more, we got a response, even with errors
-      //  }, 
-      //   error: (errMsg) => {//errMsg represents the error datd that we get from the http post observable
-      //    console.log(errMsg);
-      //    this.error= errMsg
-      //    this.isLoading=false
-      //   }
-      // });
-      ///////////////////////////////////////////////////////////////////////
     }
 
-
-
-    // this is a trick to just reduce code, since we have the same logic for subscribing in both functions of the service (signUp(), login()) we can define an observable which is a generic type so we can define which data it will yield eventually, then after both IFs we subscribe to this observable, knowing that on of the IFs will be true therefore get executed , therefore one of these expression will get assigned
-        //this.authService.login(email,password)
-        //this.authService.signUp(email, password)
-    //so one will replace the variable/Observable : authObs, then after the IFs, it is the turn for: 
-    //authObs.subscribe({ }) to get executed replacing authObs with one of the two expression, so we get the same old logic that we know of Observable and subscribe(), that is why we put authObs.subscribe({ }) after the IFs
     authObs.subscribe({
         next : (data) => {
             console.log(data);
-            this.isLoading = false
+            this.isLoading = false;
+            this.router.navigate(['/recipes']);
+            
+
         },
         error: (errMsg) => {
             console.log(errMsg);
@@ -88,6 +74,3 @@ export class AuthComponent implements OnInit {
     
   }
 }
-
-//The if condition checks if the authForm is not valid using the valid property of the form. If the form is not valid, it means that one or more form controls have failed their validation rules. In this case, the return statement is encountered, and the method execution is immediately stopped, preventing the code below from executing.
-//By returning early in this case, you can avoid executing any further logic or making any unnecessary API calls if the form data is invalid. It helps to ensure that the subsequent code is only executed when the form is in a valid state.
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,36 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from "@angular/core";
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
+ 
+    isAuthenticated = false;
+
+    private userSub : Subscription;
   
-    constructor(private dataStorageService : DataStorageService){}
+    constructor(private dataStorageService : DataStorageService,
+                private authService: AuthService){}
+
+
+    ngOnInit() {
+       this.userSub =  this.authService.user.subscribe(// this subscribe will recieve a user with value or user with null
+        (user)=>{
+            this.isAuthenticated = !user? false : true;
+           //the same as above// this.isAuthernticated = !!user;
+           console.log(!user)
+           console.log(!!user)
+        }
+       )// as mentioned in the AuthService, let us rely on the fact that this user id either null if we are not logged in or exists if we are logged in, so if we have a user we are logged in.
+       
+       
+
+    }
 
     onSaveData(){
        this.dataStorageService.storeRecipes()
@@ -18,6 +40,10 @@ export class HeaderComponent {
         //the subscribe here has been added in the sake of resolver //in subscribr() you dont have to pass in a function if you dont care about the response anyway
         this.dataStorageService.fetchRecipes().subscribe()
         
+    }
+
+    ngOnDestroy() {
+       this.userSub.unsubscribe()
     }
 }
 
