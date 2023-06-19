d2e927f2c8c4f2f18441fff8f1acfbb4b0ac35c5
karam <karambdad@gmil.com>
2023-06-19 19:27:44 +0200

    auth-306

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -41,6 +41,10 @@ export class HeaderComponent implements OnInit, OnDestroy{
         this.dataStorageService.fetchRecipes().subscribe()
         
     }
+    onLogout(){
+        this.authService.logout();
+        // it would be nice we if we redirected user and now here I'll not do it in the component but in my service (AuthService) because whilst I only have one place for logging in in my application, which is the auth component, there are multiple possible sources which could lead to a log out. It's not just the header component. We will also add logic to automatically log us out once the token expired soon. And therefore, I wanna redirect here in the logout method in my service (AuthService), in this method here logOut(){} and not in a component.
+    }
 
     ngOnDestroy() {
        this.userSub.unsubscribe()
